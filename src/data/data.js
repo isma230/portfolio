7b617b502197c31adf5a311a4fdd6dbca63115e8
ismail <esmailchari123@gmail.com>
2023-08-21 23:20:50 +0100

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+
+export const data = [
+  {
+    id: 1,
+    img:"images/image1.png",
+    title: "Full e-commerce WebSite",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'images/memory.png',
+    title: "Memory Game",
+    link: "",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'images/twitter.png',
+    title: "Twitter Clone(front-end/back-end)",
+    desc: "",
+    link: "",
+    git: "",
+  }
+  
+];
